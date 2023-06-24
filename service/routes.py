1d079d126e060e1612359b93536d546883e21e38
equip2lead <equip2lead@gmail.com>
2023-06-23 21:58:39 -0400

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -67,9 +67,11 @@ def list_accounts():
     List all Accounts
     This endpoint will list all Accounts
     """
+    
     app.logger.info("Request to list Accounts")
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
+    
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
@@ -83,12 +85,11 @@ def get_accounts(account_id):
     This endpoint will read an Account based the account_id that is requested
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
+    
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
-
-
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -114,28 +115,27 @@ def update_accounts(account_id):
 ######################################################################
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-    def delete_accounts(account_id):
-        """
-        Delete an Account
-        This endpoint will delete an Account based on the account_id that is requested
-        """
-        app.logger.info("Request to delete an Account with id: %s", account_id)
-        account = Account.find(account_id)
-        if account:
-            account.delete()
-        return "", status.HTTP_204_NO_CONTENT
-
+def delete_accounts(account_id):
+    """
+    Delete an Account
+    This endpoint will delete an Account based on the account_id that is requested
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+    return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
 
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
     if content_type and content_type == media_type:
         return
+    
     app.logger.error("Invalid Content-Type: %s", content_type)
     abort(
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
